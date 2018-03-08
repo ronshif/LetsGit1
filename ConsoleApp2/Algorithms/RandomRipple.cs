fd99a00341a21a8e9da9609b4e187a4aa552e320
Roman <ronshif@gmail.com>
2018-03-08 15:58:48 +0200

    dd

---
+++ ConsoleApp2/Algorithms/RandomRipple.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartCoin
+{
+    public class RandomAlgorithm:IAlgorithm
+    {
+        public string GetRelevantDataForOutput()
+        {
+           return  AllSitesManager.Bitfinex.GetCoinsState();
+        }
+
+        public ISitesManager AllSitesManager { get; set; }
+
+        public List<SiteName> SitesOfInterest { get; set; } = new List<SiteName>() {SiteName.BitFinex };
+        public List<CoinName> CoinsOfinterest { get; set; } = new List<CoinName>() {CoinName.Ripple };
+
+        public BitAction GetRecommendedAction()
+        {
+            Random rnd = new Random();
+            BitActionType actionType = BitActionType.Nothing;
+            double val = rnd.NextDouble();
+            if (val > 0.9)
+            {
+                actionType = BitActionType.Buy;
+            }
+
+            if (val < 0.1)
+            {
+                actionType = BitActionType.Sell;
+            }
+                
+            return new BitAction(actionType, SiteName.BitFinex, double.MaxValue);
+        }
+
+    }
+}
---
+++ ConsoleApp2/BasicTypes/FullCoinData.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartCoin
+{
+    public class FullCoinData
+    {
+        public CoinName CoinName { get; set; }
+        public List<CoinInfo> CoinHistory { get; set; } = new List<CoinInfo>();
+        public double AmountOfCoins { get; set; }
+
+        public void AddCoin(CoinInfo coin)
+        {
+            CoinHistory.Add(coin);
+        }
+
+        public CoinInfo GetCoinAtTime(DateTime dateTime)
+        {
+            for (int i = 1; i < CoinHistory.Count; i++)
+            {
+                if (CoinHistory[i].date > dateTime)
+                {
+                    return CoinHistory[i - 1];
+                }
+            }
+
+            return null;
+
+        }
+
+        public List<CoinInfo> GetCoinHistory(DateTime from,DateTime to)
+        {
+            return CoinHistory.Where(coinInfo=> coinInfo.date.CompareTo(from)>0 && coinInfo.date.CompareTo(to)<=1).ToList<CoinInfo>();
+        }
+        public List<CoinInfo> GetCoinHistory(DateTime to)
+        {
+            return GetCoinHistory(DateTime.MinValue,to);
+        }
+
+        public double GetLastCoinValue()
+        {
+            if (CoinHistory.Count == 0)
+            {
+                return 0;
+            }
+
+            return this.CoinHistory.Last().val;
+        }
+
+        public void PrintAllData()
+        {
+            foreach (CoinInfo coinInfo in CoinHistory)
+            {
+                Console.WriteLine(coinInfo);
+            }
+        }
+    }
+}
