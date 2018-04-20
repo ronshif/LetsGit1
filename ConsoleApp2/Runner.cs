4b915e20914f5c074b7348811b72b5901b43b580
romans <ronshif@gmail.com>
2018-04-20 19:38:37 +0300

    sss

--- ConsoleApp2/Runner.cs
+++ ConsoleApp2/Runner.cs
@@ -6,6 +6,7 @@ namespace SmartCoin
 {
     public class Runner
     {
+		//check
         public void RunnerSimulation(IAlgorithm algorithm)
         {
             ExchangesServicesSimulator exchangesServicesSimulator = new ExchangesServicesSimulator();
